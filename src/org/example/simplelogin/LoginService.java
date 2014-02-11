a47443f797f386e9da171be9bfe7ec43eaaeb2f5
jengDizort <jengDizort@jengDizort-PC>
2014-02-12 03:22:54 +0700

    New Commit

---
+++ src/org/example/simplelogin/LoginService.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.example.simplelogin;
+
+/**
+ *
+ * @author jengDizort
+ */
+public class LoginService {
+    private static final String DUMMY_USER_NAME = "guest";
+    private static final String DUMMY_PASSWORD = "password";
+    
+    public boolean login(String userName,String password){
+        boolean loginSuccessful = false;
+        if(userName.equals(DUMMY_USER_NAME) && password.equals(DUMMY_PASSWORD)){
+            loginSuccessful = true;
+        }
+        return loginSuccessful;
+    }
+}
